f43e2185df514bd212b9798f8b0b2d5487cd2c39
RattMoth <mattroth.dev@gmail.com>
2021-05-01 14:32:50 -0700

    Fix: Re-add localstorage. Change default to dark

--- src/hooks/useDarkMode.js
+++ src/hooks/useDarkMode.js
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react';
 import useMedia from './useMedia';
 
 export default () => {
-  const [theme, setTheme] = useState('light');
+  const [theme, setTheme] = useState('dark');
 
   const prefersDarkMode = useMedia(
     ['(prefers-color-scheme: dark)'],
@@ -12,30 +12,30 @@ export default () => {
 
   const toggleTheme = () => {
     if (theme === 'light') {
-      // if (!prefersDarkMode) {
-      // window.localStorage.setItem('theme', 'dark');
-      // }
+      if (!prefersDarkMode) {
+        window.localStorage.setItem('theme', 'dark');
+      }
       setTheme('dark');
     } else {
-      // if (!prefersDarkMode) {
-      // window.localStorage.setItem('theme', 'light');
-      // }
+      if (!prefersDarkMode) {
+        window.localStorage.setItem('theme', 'light');
+      }
       setTheme('light');
     }
   };
 
   useEffect(() => {
     if (prefersDarkMode) {
-      // window.localStorage.removeItem('theme');
+      window.localStorage.removeItem('theme');
       setTheme('dark');
     } else {
-      // const localTheme = window.localStorage.getItem('theme');
-      // if (localTheme) {
-      //   window.localStorage.setItem('theme', localTheme);
-      //   setTheme(localTheme);
-      // } else {
-      //   setTheme('light');
-      // }
+      const localTheme = window.localStorage.getItem('theme');
+      if (localTheme) {
+        window.localStorage.setItem('theme', localTheme);
+        setTheme(localTheme);
+      } else {
+        setTheme('light');
+      }
       setTheme('light');
     }
   }, [prefersDarkMode]);
