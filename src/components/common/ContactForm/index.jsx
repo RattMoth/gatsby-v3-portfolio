25519952b95e3acfdba774a01b16333acb623726
RattMoth <mattroth.dev@gmail.com>
2021-05-06 01:10:42 -0700

    Fix: Refactor ContactForm to hooks, fix submit func

--- src/components/common/ContactForm/index.jsx
+++ src/components/common/ContactForm/index.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable */
-import React from 'react';
+import React, { useState, useRef } from 'react';
 import { Form } from './styles';
 
 const encode = (data) =>
@@ -7,95 +7,90 @@ const encode = (data) =>
     .map((key) => `${encodeURIComponent(key)}=${encodeURIComponent(data[key])}`)
     .join('&');
 
-class ContactForm extends React.Component {
-  constructor(props) {
-    super(props);
-    this.ContactForm = React.createRef();
-    this.state = { name: '', email: '', subject: '', message: '' };
-  }
+const ContactForm = () => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [subject, setSubject] = useState('');
+  const [message, setMessage] = useState('');
 
-  handleSubmit = (e) => {
+  const handleSubmit = (e) => {
     e.preventDefault();
-    const form = this.ContactForm.current;
     fetch('/', {
       method: 'POST',
       headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-      body: encode({ 'form-name': form.getAttribute('name'), ...this.state }),
+      body: encode({
+        'form-name': 'contact',
+        name,
+        email,
+        subject,
+        message,
+      }),
     })
       .then(() => navigate('/'))
       .catch((error) => alert(error));
-
-    this.setState({
-      name: '',
-      email: '',
-      subject: '',
-      message: '',
-    });
   };
 
-  handleChange = (e) => {
-    return this.setState({ [e.target.name]: e.target.value });
-  };
-
-  render() {
-    const { name, email, subject, message } = this.state;
-    return (
-      <Form>
-        <form
-          className="cf"
-          method="post"
-          name="contact"
-          onSubmit={this.handleSubmit}
-          data-netlify="true"
-          data-netlify-honeypot="bot-field"
-        >
-          <div className="half left cf">
-            <input type="hidden" name="form-name" value="contact" />
-            <input
-              required
-              type="text"
-              name="name"
-              value={name}
-              id="input-name"
-              placeholder="Name"
-              onChange={this.handleChange}
-            />
-            <input
-              required
-              type="email"
-              name="email"
-              value={email}
-              id="input-email"
-              autoComplete="true"
-              placeholder="Email address"
-              onChange={this.handleChange}
-            />
-            <input
-              required
-              type="text"
-              name="subject"
-              value={subject}
-              id="input-subject"
-              placeholder="Subject"
-              onChange={this.handleChange}
-            />
-          </div>
-          <div className="half right cf">
-            <textarea
-              required
-              name="message"
-              type="text"
-              value={message}
-              id="input-message"
-              placeholder="Message"
-              onChange={this.handleChange}
-            />
-          </div>
-          <input type="submit" id="input-submit" value="Submit Message" />
-        </form>
-      </Form>
-    );
-  }
-}
+  return (
+    <Form>
+      <form
+        className="cf"
+        method="post"
+        name="contact"
+        onSubmit={handleSubmit}
+        data-netlify="true"
+        data-netlify-honeypot="bot-field"
+      >
+        <div className="half left cf">
+          <input type="hidden" name="form-name" value="contact" />
+          <input
+            required
+            type="text"
+            name="name"
+            value={name}
+            id="input-name"
+            placeholder="Name"
+            onChange={(e) => setName(e.target.value)}
+          />
+          <input
+            required
+            type="email"
+            name="email"
+            value={email}
+            id="input-email"
+            autoComplete="true"
+            placeholder="Email address"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input
+            required
+            type="text"
+            name="subject"
+            value={subject}
+            id="input-subject"
+            placeholder="Subject"
+            onChange={(e) => setSubject(e.target.value)}
+          />
+        </div>
+        <div className="half right cf">
+          <textarea
+            required
+            name="message"
+            type="text"
+            value={message}
+            id="input-message"
+            placeholder="Message"
+            onChange={(e) => setMessage(e.target.value)}
+          />
+        </div>
+        <input
+          type="submit"
+          id="input-submit"
+          name="submit"
+          value="Submit Message"
+        />
+      </form>
+    </Form>
+  );
+};
 
 export default ContactForm;
