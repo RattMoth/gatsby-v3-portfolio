15525d490d1fde338865db573cf13957622fbfd3
RattMoth <mattroth.dev@gmail.com>
2021-12-29 22:31:24 -0800

    Use boostrap tooltips and remove localstorage
    Bootstrap tooltips used in place of react-tooltips.
    
    Localstorage no longer used to store theme info.

--- src/components/theme/Header/ToggleTheme/index.jsx
+++ src/components/theme/Header/ToggleTheme/index.jsx
@@ -1,49 +1,52 @@
-import React, { useRef, useContext, useEffect } from 'react';
-import ReactTooltip from 'react-tooltip';
+import React, { useContext, useEffect, useState } from 'react';
+import Tooltip from 'react-bootstrap/Tooltip';
+import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
 import sunIcon from '../../../../assets/icons/sun.svg';
 import moonIcon from '../../../../assets/icons/moon.svg';
 import { ThemeContext } from '../../../../providers/ThemeProvider';
 import { Wrapper } from './styles';
 
+const renderTooltip = (props) => (
+  // eslint-disable-next-line react/jsx-props-no-spreading
+  <Tooltip id="theme-tooltip" {...props}>
+    Your device auto-selected dark mode.
+  </Tooltip>
+);
+
 const ToggleTheme = () => {
   const { theme, toggleTheme } = useContext(ThemeContext);
-  const themeRef = useRef();
+  const [themeTooltipWillShow, setThemeTooltipWillShow] = useState(false);
 
-  const showTooltip = () => {
-    ReactTooltip.show(themeRef.current);
+  const flashTooltip = () => {
+    setTimeout(() => {
+      setThemeTooltipWillShow(true);
+      setTimeout(() => setThemeTooltipWillShow(false), 3000);
+    }, 1500);
   };
 
   const handleClick = () => {
     toggleTheme();
-    ReactTooltip.hide(themeRef.current);
   };
 
   useEffect(() => {
     const prefersDarkMode = window.matchMedia('(prefers-color-scheme: dark)')
       .matches;
-    if (prefersDarkMode) {
-      showTooltip();
+
+    if (prefersDarkMode && theme === 'dark') {
+      flashTooltip();
     }
-  }, []);
+  }, [theme]);
 
   return (
     <Wrapper type="button" onClick={handleClick}>
-      <ReactTooltip
-        style={{ width: '15ch !important' }}
-        html
-        backgroundColor="#2f3435"
-        delayShow={2000}
-        place="bottom"
-        id="theme-tooltip"
-      />
-      <img
-        ref={themeRef}
-        src={theme === 'dark' ? sunIcon : moonIcon}
-        alt={theme}
-        data-for="theme-tooltip"
-        data-tip="Your device auto-selected dark mode."
-        // data-tip="Lorem ipsum dolor sit amet consectetur adipisicing elit. Et modi officia expedita nisi, quasi quae."
-      />
+      <OverlayTrigger
+        show={themeTooltipWillShow}
+        placement="bottom"
+        delay={{ show: 2250, hide: 400 }}
+        overlay={renderTooltip}
+      >
+        <img src={theme === 'dark' ? sunIcon : moonIcon} alt={theme} />
+      </OverlayTrigger>
     </Wrapper>
   );
 };
