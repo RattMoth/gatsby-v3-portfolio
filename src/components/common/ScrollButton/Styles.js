c5a5252d9c52323d567049190064a54003f07b64
RattMoth <mattroth.dev@gmail.com>
2022-01-10 22:05:19 -0800

    Fix scrollbutton position on small screens

--- src/components/common/ScrollButton/Styles.js
+++ src/components/common/ScrollButton/Styles.js
@@ -3,10 +3,14 @@ import styled from 'styled-components';
 export const Button = styled.div`
   position: fixed;
   width: 100%;
-  left: 95%;
+  left: 95vw;
   bottom: 40px;
   height: 20px;
   font-size: 2.25rem;
   z-index: 1;
   cursor: pointer;
+
+  @media (max-width: 830px) {
+    left: 90vw;
+  }
 `;
